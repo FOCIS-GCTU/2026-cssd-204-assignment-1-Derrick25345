c790c4dac8c861faaf21b184f7f60cf5b55e620c
Derrick25345 <derrickelikem63@gmail.com>
2026-05-31 22:52:47 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: a program that prints my initials in massive block letters using 10prints stament written in a function
+# Assignment Number: 1
+#
+# Name: AWI DERRICK ELIKEM
+# STUDENT ID: 2425403905
+# Email: 2425403905@gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, AWI DERRICK ELIKEM, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
+def main(): # defined function to print my initials
   pass #code goes here
+  print()
+  print("...ADE")
+  print()
+  print(".....AAAAAAAA..........DDDDDDDDDDDD........EEEEEEEEEEEE...")
+  print("....AAAAAAAAAA.........DDDDDDDDDDDD........EEEEEEEEEEEE...")
+  print("...AAAA....AAAA........DDDD.......D........EEEE...........")
+  print("...AAAA....AAAA........DDDD.......D........EEEE...........")
+  print("...AAAAAAAAAAAA........DDDD.......D........EEEEEEEEEEEE...")
+  print("...AAAA....AAAA........DDDD.......D........EEEEEEEEEEEE...")
+  print("...AAAA....AAAA........DDDD.......D........EEEE...........")
+  print("...AAAA....AAAA........DDDD.......D........EEEE...........")
+  print("...AAAA....AAAA........DDDD......D.........EEEE...........")
+  print("...AAAA....AAAA...**...DDDDDDDDDD.....**...EEEEEEEEEEEE...")
+  print("...AAAA....AAAA...**...DDDDDDDDD......**...EEEEEEEEEEEE...")
+  print()
+  
 
-
-
-main()
+main() 
